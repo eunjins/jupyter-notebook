5c1aa6a4e51d906196db6616c84eea64d36c2425
eunjins <jklkm90@gmail.com>
2021-03-05 18:58:48 +0900

    benchmark code

---
+++ inference/benchmark.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main() {
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result0.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result1.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result2.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result3.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result4.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result5.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result6.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result7.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result8.txt");
+    system("sudo python3 benchmark.py --all --csv_file_path /home/eunjin/jetson_benchmarks/benchmark_csv/tx2-nano-benchmarks.csv \
+                            --model_dir /home/eunjin/jetson_benchmarks/models \
+                            --jetson_devkit nano \
+                            --gpu_freq 921600000 --power_mode 0 > /home/eunjin/benchmark_result/result9.txt");                     
+}
