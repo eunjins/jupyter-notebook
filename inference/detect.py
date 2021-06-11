c6c6e1100a85aaba17a68ec5708a244b2c29f927
eunjins <jklkm90@gmail.com>
2021-06-11 21:50:54 +0900

    add: about what I have to do

---
+++ inference/detect.py
@@ -0,0 +1,15 @@
+'''
+classify -> detect.
+
+Something to add?
+'''
+
+# Specify the TensorFlow model, labels, and image
+
+# Initialize the TF interpreter
+
+# Resize the image
+
+# Run an inference
+
+# Print the result
